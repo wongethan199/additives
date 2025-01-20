3183b1626e143cace24b8ce419bfc4a730ea4af2
wongethan199 <wongethan199@gmail.com>
2025-01-20 17:15:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
 st.title("additives")
 limits={}#dictionary for limits for additives
-pandas.read_csv("additives.csv")
+x=pandas.read_csv("additives.csv")
+y=int(st.text_input("Enter number of unique food items to test"))
+additives=[]
+for i in range(y):
+  z=st.selectbox("Enter food item "+str(i),x["Food"].values.tolist())
+  num=int(st.text_input("Enter quantity))
+  target=x[x["Food"]==z]
+  #add values of additives
+for i in range():
+  st.write(additives[i],"of",dic[i])
