a525c67b433a8a102f607126a016d28ea86d3590
wongethan199 <wongethan199@gmail.com>
2025-01-22 10:58:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,12 @@ st.header("additives")
 import pandas
 import csv
 "Data provided are very rough averages of a few brands, and may contain dyes that are not present in a specific brand. Exact values are unknown, but some data is available on https://www.kaggle.com/datasets/sujaykapadnis/certified-food-dye-levels-in-food-samples"
-limits=[5,3.75,7,2.5,12]
+standards=st.selectbox("Enter standards to compare",["USFDA","JECFA","EFSA"])
+limits=[7.5,4,7,0.1,6]
+if standards=="USFDA":
+  limits=[5,3.75,7,2.5,12]
+if standards=="EFSA":
+  limits[4]=12.5
 x=pandas.read_csv("additives.csv")
 try:
   weight=float(st.text_input("Enter body weight in kg, defaults to 60"))
@@ -19,7 +24,7 @@ if y:
   dic=["Yellow 5","Yellow 6","Red 40","Red 3","Blue 1"]
   for i in range(y):
     z=st.selectbox("Enter food item "+str(i+1),x["Food"].values.tolist())
-    num=int(st.text_input(f"Enter quantity of food item {i+1} in g"))
+    num=int(st.text_input(f"Enter quantity of food item {i+1} consumed in a day in g"))
     target=x[x["Food"]==z]
     target=list(target.iloc[0])
     for j in range(1,6):
